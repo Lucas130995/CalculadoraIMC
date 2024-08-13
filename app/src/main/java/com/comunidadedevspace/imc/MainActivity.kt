55b6aee5d7b923c23df16661672091ded19d832e
Lucas <lvc_ptbr@hotmail.com>
2024-08-13 19:19:00 +0100

    mostrar menssagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Recuperar os componentes EditText
-            // Criar uma variavel e associar o componente de UI <EditText>
+        // Criar uma variavel e associar o componente de UI <EditText>
         // Recuperar o botao da tela
 
         // Colocar acao no botao setOnClickListener
@@ -23,14 +24,30 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString(). toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
 
-            println("Lucas Botton" + resultado )
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "")
+            //Mostrar mensagem para o usuario
+
+                Snackbar.make(
+                    edtPeso,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+            else {
+
+                val peso = edtPeso.text.toString().toFloat()
+                val altura = edtAltura.text.toString().toFloat()
+
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                println("Lucas Botton" + resultado)
+            }
         }
-
-    }
-}
+    }}
