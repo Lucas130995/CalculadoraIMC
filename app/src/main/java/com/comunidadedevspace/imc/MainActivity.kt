fba2b902dc474f934e45b053037ceeada970874e
Lucas <lvc_ptbr@hotmail.com>
2024-08-15 16:58:02 +0100

    Resultado do IMC na proxima tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
@@ -10,13 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Recuperar os componentes EditText
-        // Criar uma variavel e associar o componente de UI <EditText>
-        // Recuperar o botao da tela
-
-        // Colocar acao no botao setOnClickListener
-        // Recuperar o texto digitado no edt peso
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
@@ -29,25 +24,30 @@ class MainActivity : AppCompatActivity() {
             val pesoStr: String = edtPeso.text.toString()
             val alturaStr: String = edtAltura.text.toString()
 
-            if (pesoStr == "" || alturaStr == "")
-            //Mostrar mensagem para o usuario
-
-                Snackbar.make(
-                    edtPeso,
-                    "Preencha todos os campos",
-                    Snackbar.LENGTH_LONG
-                )
+            if (pesoStr == "" || alturaStr == "") {
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
                     .show()
-            else {
+            } else {
 
-                val peso = edtPeso.text.toString().toFloat()
-                val altura = edtAltura.text.toString().toFloat()
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
 
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
 
-                println("Lucas Botton" + resultado)
+                println("Lucas acao do botao" + resultado)
+
+                val intent = Intent(this, ResultActivity2::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+                println("Lucas acao do Botton" + resultado)
             }
         }
     }}
